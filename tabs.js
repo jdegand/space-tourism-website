67a15b28040d557f0e748731f9ceda57bd4e7a21
J. Degand <70610011+jdegand@users.noreply.github.com>
2024-09-06 18:13:12 +0000

    fix: remove commented out code

--- tabs.js
+++ tabs.js
@@ -8,41 +8,14 @@ tabList.addEventListener('keydown', changeTabFocus);
 tabs.forEach((tab) => {
     tab.addEventListener('click', changeTabPanel);
 })
-/*
-function changeTabFocus(e) {
-    const keydownLeft = 37;
-    const keydownRight = 39;
-    
-    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
-        tabs[tabFocus].setAttribute("tabindex", -1);
-    }
-    
-    if (e.keyCode === keydownRight) {
-        tabFocus++;
-        if (tabFocus >= tabs.length) {
-            tabFocus = 0;
-        }
-    }
-    
-    if (e.keyCode === keydownLeft) {
-        tabFocus--;
-        if (tabFocus < 0) {
-            tabFocus = tabs.length - 1;
-        }
-    }
-    
-    tabs[tabFocus].setAttribute("tabindex", 0);
-    tabs[tabFocus].focus();
-}
-*/
 
 function changeTabFocus(e) {
     const keydownLeft = 37;
     const keydownRight = 39;
-    
+
     if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
         tabs[tabFocus].setAttribute("tabindex", -1);
-        
+
         if (e.keyCode === keydownRight) {
             tabFocus++;
             if (tabFocus >= tabs.length) {
@@ -54,7 +27,7 @@ function changeTabFocus(e) {
                 tabFocus = tabs.length - 1;
             }
         }
-        
+
         tabs[tabFocus].setAttribute("tabindex", 0);
         tabs[tabFocus].focus();
     }
@@ -71,26 +44,13 @@ function changeTabPanel(e) {
     tabContainer.querySelector('[aria-selected="true"]').setAttribute("aria-selected", false);
 
     targetTab.setAttribute("aria-selected", true);
-    
+
     mainContainer.querySelectorAll('[role="tabpanel"]').forEach(panel => panel.setAttribute('hidden', true));
     mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
 
     //hidden not working on picture element - images still stack
-    
+
     mainContainer.querySelectorAll('picture').forEach((picture) => picture.classList.add('d-none'));
 
     mainContainer.querySelector([`#${targetImage}`]).classList.replace('d-none', 'd-block');
-    // console.log(mainContainer);
 }
-
-/*
-function hideContent(parent, content) {
-    parent
-        .querySelectorAll(content)
-        .forEach((item) => item.setAttribute("hidden", true));
-}
-
-function showContent(parent, content) {
-     parent.querySelector(content).removeAttribute('hidden');
-}
-*/
